995dc480a902f272621c2afb072be71a79fb67ce
Jakub Sancho Gożuk <steamgozuk@gmail.com>
2024-12-03 08:44:02 +0100

    Struktura plików, szkielet funkcji, import bibliotek

---
+++ user_management.py
@@ -0,0 +1,42 @@
+import json
+import random
+import re
+import os
+
+# Create non-existent directories
+if not os.path.exists("data"): os.makedirs("data")
+
+def add_user(user_data):
+    '''
+    Dodaje nowego użytkownika
+    '''
+
+def remove_user(user_id):
+    '''
+    Usuwa istniejącego użytkownika
+    '''
+
+def edit_user(user_id, updated_data):
+    '''
+    Edytuj dane użytkownika
+    '''
+
+def validate_nip(nip):
+    '''
+    Waliduje numer NIP
+    '''
+
+def validate_regon(regon):
+    '''
+    Waliduje numer REGON
+    '''
+
+def generate_password():
+    '''
+    Generuje silne hasło
+    '''
+
+def validate_password(password):
+    '''
+    Waliduje silne hasło
+    '''
